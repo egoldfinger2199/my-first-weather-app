35c972397ec05577fd4db01f42d41464f4a8fc0d
egoldfinger2199 <goldfinger.evawray@gmail.com>
2020-07-13 11:39:50 -0400

    Implemented 3-hour forecast

--- src/script.js
+++ src/script.js
@@ -1,14 +1,6 @@
-function formatDate(date) {
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
 
-  let dayIndex = date.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -18,13 +10,28 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  let day = days[dayIndex];
 
-  return `${day} ${hours}:${minutes}`;
+  let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
 }
 
 function displayWeatherCondition(response) {
   let temperatureElement = document.querySelector("#temperature");
+  let dateElement = document.querySelector("#date");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
@@ -40,6 +47,7 @@ function displayWeatherCondition(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   minimumTemperatureElement.innerHTML = Math.round(response.data.main.temp_min);
   maximumTemperatureElement.innerHTML = Math.round(response.data.main.temp_max);
   iconElement.innerHTML = response.data.weather[0].icon;
@@ -48,7 +56,7 @@ function displayWeatherCondition(response) {
   } else if (response.data.weather[0].icon === "02d") {
     iconElement.innerHTML = `<i class="fas fa-cloud-sun"></i>`;
   } else if (response.data.weather[0].icon === "03d") {
-    iconElement.innerHTML = `<i class="fas fa-cloud"></i>`;
+    iconElement.innerHTML = `<i class="fas fa-cloud-sun"></i>`;
   } else if (response.data.weather[0].icon === "04d") {
     iconElement.innerHTML = `<i class="fas fa-cloud-sun"></i>`;
   } else if (response.data.weather[0].icon === "09d") {
@@ -76,10 +84,79 @@ function displayWeatherCondition(response) {
   }
 }
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = null;
+  forecastElement.innerHTML = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+     <div class="col-2">
+      <div class="day-of-the-week">
+          <strong>
+             ${formatHours(forecast.dt * 1000)}
+          </strong>
+      </div>
+      <div
+        ${getIcon(forecast.weather[0].icon)}
+      </div>
+      <div class="temperature-forecast">
+        <strong>
+        ${Math.round(forecast.main.temp_max)}°
+        </strong>
+        / ${Math.round(forecast.main.temp_min)}°
+      </div>
+    </div>
+    `;
+
+    function getIcon(icon) {
+      let iconHTML = "";
+      if (icon === "01d") {
+        iconHTML = `<i class="fas fa-sun"></i>`;
+      } else if (icon === "02d") {
+        iconHTML = `<i class="fas fa-cloud-sun"></i>`;
+      } else if (icon === "03d") {
+        iconHTML = `<i class="fas fa-cloud-sun"></i>`;
+      } else if (icon === "04d") {
+        iconHTML = `<i class="fas fa-cloud-sun"></i>`;
+      } else if (icon === "09d") {
+        iconHTML = `<i class="fas fa-cloud-sun-rain"></i>`;
+      } else if (icon === "10d") {
+        iconHTML = `<i class="fas fa-cloud-showers-heavy"></i>`;
+      } else if (icon === "11d" || "10n") {
+        iconHTML = `<i class="fas fa-poo-storm"></i>`;
+      } else if (icon === "13d" || "13n") {
+        iconHTML = `<i class="fas fa-snowflake"></i>`;
+      } else if (icon === "50d" || "50n") {
+        iconHTML = `<i class="fas fa-smog"></i>`;
+      } else if (icon === "01n") {
+        iconHTML = `<i class="fas fa-moon"></i>`;
+      } else if (icon === "02n") {
+        iconHTML = '<i class="fas fa-cloud-moon"></i>';
+      } else if (icon === "03n") {
+        iconHTML = `<i class="fas fa-cloud"></i>`;
+      } else if (icon === "04n") {
+        iconHTML = `<i class="fas fa-cloud-moon"></i>`;
+      } else if (icon === "09n") {
+        iconHTML = `<i class="fas fa-cloud-moon-rain"></i>`;
+      } else if (icon === "10n") {
+        iconHTML = `<i class="fas fa-cloud-moon-rain"></i>`;
+      } else {
+        iconHTML = `<img src="https://openweathermap.org/img/wn/${icon}@2x.png" />`;
+      }
+      return iconHTML;
+    }
+  }
+}
+
 function searchCity(city) {
   let apiKey = "0f0e97f91ce7e0b3ade2a5e918c941d0";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherCondition);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
